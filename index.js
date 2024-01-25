3133ad63daa8042078a283c7f87588877c048157
Adiar <93129221+Okarix@users.noreply.github.com>
2024-01-25 17:44:16 +0600

    example of simple authorization with jwt

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const app = express(); // вся логика приложения хранится в этой переменной
 
+app.use(express.json()); // позволяет читать json запросы
+
 app.get('/', (req, res) => {
-	res.send('Hello');
+	res.send('Hello world');
 }); // ответ на гет запрос
+
+app.post('/login', (req, res) => {
+	console.log(req.body);
+
+	if (req.body.email === 'test@gmail.com') {
+		const token = jwt.sign(
+			{
+				email: req.body.email,
+				fullName: 'Бека Беков',
+			},
+			'secret123'
+		); //генерируем токен(внутри же шифруем)
+		res.json({
+			success: true,
+			token,
+		});
+	} else {
+		res.json({
+			error: 'Incorrect email format',
+		});
+	}
+});
 
 app.listen(4444, err => {
 	if (err) {
