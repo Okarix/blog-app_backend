6f1136b54e6bf9d3ce1e72bf6df31caa20d9334b
Adiar <93129221+Okarix@users.noreply.github.com>
2024-01-28 14:16:57 +0600

    refactor: added index.js file into which files are exported controllers and utils

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
 import multer from 'multer';
-import * as PostController from './controllers/PostController.js';
-import * as UserController from './controllers/UserController.js';
-import checkAuth from './utils/checkAuth.js';
-import handleValidationErrors from './utils/handleValidationErrors.js';
+import { PostController, UserController } from './controllers/index.js';
+import { checkAuth, handleValidationErrors } from './utils/index.js';
 import { loginValidation, postCreateValidation, registerValidation } from './validatiions.js';
 
 const adminPass = process.env.ADMIN_PASS;
